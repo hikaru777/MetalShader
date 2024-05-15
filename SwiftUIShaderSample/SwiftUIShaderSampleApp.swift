c7614490e3d84f0600dfaae92aeac73f5a90b557
HikaruHonda <hikaru.h.developer@gmail.com>
2024-05-15 18:22:48 +0900

    Initial Commit

---
+++ SwiftUIShaderSample/SwiftUIShaderSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIShaderSampleApp.swift
+//  SwiftUIShaderSample
+//
+//  Created by 本田輝 on 2024/05/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIShaderSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
